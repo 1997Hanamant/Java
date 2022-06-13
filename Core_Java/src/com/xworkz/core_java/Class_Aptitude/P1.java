6ca4821913b8e41c986f25e8f80e1586f7e68690
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-06-13 13:53:11 +0530

    Class Aptitude

---
+++ Core_Java/src/com/xworkz/core_java/Class_Aptitude/P1.java
@@ -0,0 +1,18 @@
+package com.xworkz.core_java.Class_Aptitude;
+
+public class P1 {
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=4;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+	
+
+      }
+}
+// ****
+// ****
+// ****
+// ****
---
+++ Core_Java/src/com/xworkz/core_java/Class_Aptitude/P3.java
@@ -0,0 +1,18 @@
+package com.xworkz.core_java.Class_Aptitude;
+
+public class P3 {
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {
+			for(int j=5;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(int a=1;a<=i;a++) {
+				
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
