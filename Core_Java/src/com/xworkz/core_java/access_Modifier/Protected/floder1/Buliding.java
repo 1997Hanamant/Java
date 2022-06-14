f0a620073c0f004462f29eb9051fd90093605688
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-06-15 01:08:22 +0530

    Protected access Modifier

---
+++ Core_Java/src/com/xworkz/core_java/access_Modifier/Protected/floder1/Buliding.java
@@ -0,0 +1,8 @@
+package com.xworkz.core_java.access_Modifier.Protected.floder1;
+
+public class Buliding {
+	protected void oneBHKRoom() {
+		System.out.println("Buliding have one Bath,Hall,Kitceh");
+	}
+
+}
---
+++ Core_Java/src/com/xworkz/core_java/access_Modifier/Protected/floder2/ResidentialBuliding.java
@@ -0,0 +1,11 @@
+package com.xworkz.core_java.access_Modifier.Protected.floder2;
+import com.xworkz.core_java.access_Modifier.Protected.floder1.*;
+
+public class ResidentialBuliding extends Buliding{
+	public static void main(String[] args) {
+		ResidentialBuliding B1=new ResidentialBuliding();
+		B1.oneBHKRoom();
+	}
+	
+
+}
