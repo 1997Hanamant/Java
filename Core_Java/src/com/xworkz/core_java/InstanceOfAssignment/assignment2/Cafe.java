8fc9ffb8b7f61411eef8e98fb416e3e3ee154079
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-06-24 16:41:18 +0530

    InstanceOfAssignement

--- Core_Java/src/com/xworkz/core_java/InstanceOfAssignment/assignment2/Cafe.java
+++ Core_Java/src/com/xworkz/core_java/InstanceOfAssignment/assignment2/Cafe.java
@@ -1,19 +1,19 @@
 package com.xworkz.core_java.InstanceOfAssignment.assignment2;
 
 public class Cafe {
-Beverage vendingmachine(int choice)
+	Beverage vendingmachine(int choice)
 	{
-if(choice==1)
-{
-	return new Coffee();
-}
-else if(choice==2)
-{
-return new Tea();
-}
-else
-{
-	return null;
-}
-}
+		if(choice==1)
+		{
+			return new Coffee();
+		}
+		else if(choice==2)
+		{
+			return new Tea();
+		}
+		else
+		{
+			return null;
+		}
+	}
 }
