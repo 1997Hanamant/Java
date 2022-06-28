7114e37dbe59ed6a9d3bb8a196b0385f3a3732dd
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-06-29 01:23:39 +0530

    getClass

---
+++ Core_Java/src/com/xworkz/core_java/abstraction/assignment/assignment4/Student.java
@@ -0,0 +1,9 @@
+package com.xworkz.core_java.abstraction.assignment.assignment4;
+
+public class Student extends  Trainer,Trainer1{
+	public static void main(String args[]) {
+		Student s=new Student();
+		
+	}
+	
+}
---
+++ Core_Java/src/com/xworkz/core_java/javaLibraies/getclass/Phone.java
@@ -0,0 +1,10 @@
+package com.xworkz.core_java.javaLibraies.getclass;
+
+public class Phone {
+	public static void main(String[] args) {
+		Phone  phone=new Phone();
+		System.out.println(phone.getClass().getSimpleName());
+		
+	}
+
+}
