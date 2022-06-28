8fbb7806152ab6663b0ebccacd704a684b19f431
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-06-28 21:16:45 +0530

    encapsulation

--- Core_Java/src/com/xworkz/core_java/encapsulation/encapsulation_class/DogTest.java
+++ Core_Java/src/com/xworkz/core_java/encapsulation/encapsulation_class/DogTest.java
@@ -1,9 +1,14 @@
 package com.xworkz.core_java.encapsulation.encapsulation_class;
 
+import java.util.Scanner;
+
 public class DogTest {
 	public static void main(String[] args) {
 		Dog dog =new Dog();
-		dog.setName("Rocky");
+		Scanner scan=new Scanner(System.in);
+		System.out.println(" Enter a name");
+		String name=scan.nextLine();
+		//dog.setName("Rocky");
 		dog.setbreed("German Shepherd");
 		dog.setcolor("Black");
 		dog.setage(200);
