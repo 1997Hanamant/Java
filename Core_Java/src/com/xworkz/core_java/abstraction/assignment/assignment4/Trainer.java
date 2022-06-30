2d7b783a0dcf1145ef653c370d394c46a5689cae
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-07-01 00:08:04 +0530

    abstarction

---
+++ Core_Java/src/com/xworkz/core_java/abstraction/assignment/assignment4/Trainer.java
@@ -0,0 +1,8 @@
+package com.xworkz.core_java.abstraction.assignment.assignment4;
+abstract class Trainer {
+	 void Teach() {
+	 System.out.println("Hi");
+
+}
+
+}
---
+++ Core_Java/src/com/xworkz/core_java/abstraction/assignment/assignment4/Trianer1.java
@@ -0,0 +1,8 @@
+package com.xworkz.core_java.abstraction.assignment.assignment4;
+
+abstract class Trianer1 {
+	 void  Teach1() {
+		 System.out.println("Hello");
+	 }
+
+}
