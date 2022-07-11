f7ed9d2f9936018cfb8f42fc0646ac5eb97317d1
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-07-11 12:35:09 +0530

    Parsing

---
+++ Core_Java/src/com/xworkz/core_java/parsing/ParsingClass.java
@@ -0,0 +1,57 @@
+package com.xworkz.core_java.parsing;
+
+public class ParsingClass {
+	public static void main(String[] args) {
+		String s="1000a";
+		String s1="2000";
+		//String s2=s+s1;
+//		System.out.println(s2);
+//		Integer str=Integer.parseInt(s);
+//		Integer str1=Integer.parseInt(s1);
+//		System.out.println("s:"+str);
+//		System.out.println("s1:"+str1);
+//		Integer str2=str+str1;
+//		System.out.println("str2:"+str2);
+//		
+//		Byte by=Byte.parseByte(s);
+//		Byte by1=Byte.parseByte(s1);
+//		Byte by2=(byte) (by+by1);
+//		System.out.println("by2:"+by2);
+//		
+//		Short sh=Short.parseShort(s);
+//		Short sh1=Short.parseShort(s1);
+//		Short sh2= (short) (sh+sh1);
+//		System.out.println("sh2:"+sh2);
+		
+//		Long l=Long.parseLong(s);
+//	    Long l1=Long.parseLong(s1);
+//	    Long l2=l+l1;
+//	    System.out.println(l2);
+		
+		Double D=Double.parseDouble(s);
+		Double D1=Double.parseDouble(s1);
+		Double D2=D+D1;
+		System.out.println("D2:"+D2);
+		
+		Float f=Float.parseFloat(s);
+		Float f1=Float.parseFloat(s1);
+		Float f2=f+f1;
+		System.out.println(f2);
+		
+	//	String r="true";
+	//	String r1="flase";
+	//	Boolean b=Boolean.parseBoolean(r);
+	//	Boolean b1=Boolean.parseBoolean(s2);
+	//	System.out.println("r:"+r);
+	//	System.out.println("r1:"+r1);
+	//	System.out.println("b:"+b);
+	//	System.out.println("b1:"+b1);
+
+
+
+	}
+
+
+
+}
+
