619993397c19ceab6f8a90cfa05469bbb48420b0
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-07-19 00:09:56 +0530

    DAO and DTO

--- Core_Java/src/com/xworkz/core_java/dtoandDao/dao/StudentDAO.java
+++ Core_Java/src/com/xworkz/core_java/dtoandDao/dao/StudentDAO.java
@@ -3,6 +3,7 @@ package com.xworkz.core_java.dtoandDao.dao;
 import com.xworkz.core_java.dtoandDao.dto.StudentDTO;
 
 public interface StudentDAO {
+	int total_Size=10;
 	void addStudent(StudentDTO dto);
     StudentDTO getStudent(long phoneNo);
     void updateStudent(StudentDTO dto);
