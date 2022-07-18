df9af64e25f281b88f2487614200ef9f9ec30ef8
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-07-19 00:09:09 +0530

    Java libraies

--- Core_Java/src/com/xworkz/core_java/javaLibraies/finalizeMethod/FinalizeDemo.java
+++ Core_Java/src/com/xworkz/core_java/javaLibraies/finalizeMethod/FinalizeDemo.java
@@ -2,7 +2,7 @@ package com.xworkz.core_java.javaLibraies.finalizeMethod;
 
 public class FinalizeDemo {
 	public static void main(String[] args) throws Throwable {
-		new Biriyani("Malappa",30.00).finalize();
+	      new Biriyani("Malappa",30.00).finalize();
 		//System.gc();
 		//biriyani=null;
 		//System.out.println(biriyani);
