5db644b193357ce6c77cd23d4c58e0dadf732be9
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-07-19 00:17:59 +0530

    ArryList

---
+++ Core_Java/src/com/xworkz/core_java/collectionFramework/ArryListDemo.java
@@ -0,0 +1,39 @@
+package com.xworkz.core_java.collectionFramework;
+import java.util.ArrayList;
+
+public class ArryListDemo {
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		al.add("Java");
+		al.add(null);
+		System.out.println(al);
+		System.out.println(al.size());
+		System.out.println(al.get(2));
+		System.out.println(al.contains("Java"));
+		System.out.println(al.contains("java"));
+		System.out.println(al);
+		System.out.println(al.remove(2));
+		System.out.println(al);
+		System.out.println(al.isEmpty());
+		al.clear();
+		System.out.println(al.isEmpty());
+		ArrayList l=new ArrayList();
+		l.add(50);
+		l.add(200);
+		l.add(100);
+		l.add(200);
+	    l.add(300);
+	    l.add(200);
+	    System.out.println(l.indexOf(100));
+	    System.out.println(l.indexOf(200));
+	    System.out.println(l.lastIndexOf(200));
+		
+		
+	}
+
+}
