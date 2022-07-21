b39ab8ec621c3d1dcce4100eeefc5fc6324e90c1
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-07-21 12:25:00 +0530

    DTo and Dao

--- Core_Java/src/com/xworkz/core_java/dtoAndDtoAssignment/assignment1/dao/EmplyoeeDaoImpl.java
+++ Core_Java/src/com/xworkz/core_java/dtoAndDtoAssignment/assignment1/dao/EmplyoeeDaoImpl.java
@@ -39,7 +39,7 @@ public class EmplyoeeDaoImpl implements EmplyoeeDao {
 				break;
 			}
 		}
-		
+
 	}
 
 	@Override
@@ -52,6 +52,6 @@ public class EmplyoeeDaoImpl implements EmplyoeeDao {
 				break;
 			}
 
+		}
 	}
-}
 }
