bcc33710cca1a6bf4eddc18c3afb24785c8a81cd
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-07-22 23:43:05 +0530

    HashSet

---
+++ Core_Java/src/com/xworkz/core_java/collectionFramework/set/hashset/HashSetDemo.java
@@ -0,0 +1,21 @@
+package com.xworkz.core_java.collectionFramework.set.hashset;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+	public static void main(String[] args) {
+		HashSet hs=new HashSet();
+		hs.add(2);
+		hs.add(1);
+		hs.add(1);
+		hs.add("null");
+		hs.add("null");
+		hs.add("Java");
+		
+		System.out.println(hs);
+		
+				
+		
+	}
+
+}
