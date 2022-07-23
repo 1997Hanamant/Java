aa7353cd5978738c8587a77c3811a25c7eabb4d0
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-07-23 18:01:11 +0530

    StringBuffer

--- Core_Java/src/com/xworkz/core_java/string_Class/StringBufferDemo.java
+++ Core_Java/src/com/xworkz/core_java/string_Class/StringBufferDemo.java
@@ -5,16 +5,16 @@ public class StringBufferDemo {
 		StringBuffer Bb=new StringBuffer("Hubli");
 		Bb.append("City");
 		System.out.println(Bb);
-	//System.out.println(	Bb.delete(1, 4));
-	//System.out.println(Bb.capacity());
-	//System.out.println(Bb.equals(Bb));
-	//System.out.println(Bb.insert(2, 2));
-	//System.out.println(Bb.insert(1, 2.2));
-	//System.out.println(Bb.replace(1, 6, "as"));
-	//	System.out.println(Bb.reverse());
+//		System.out.println(	Bb.delete(1, 4));
+//		System.out.println(Bb.capacity());
+//		System.out.println(Bb.equals(Bb));
+//		System.out.println(Bb.insert(2, 2));
+//		System.out.println(Bb.insert(1, 2.2));
+//		System.out.println(Bb.replace(1, 6, "as"));
+//		System.out.println(Bb.reverse());
 
-	
-		
+
+
 	}
 
 }
