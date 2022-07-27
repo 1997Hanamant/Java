e0c631020866162e8105d99d8f0b21ad49a24997
1997Hanamant <hanamantc.xworkz@gmail.com>
2022-07-27 21:37:05 +0530

    Map

---
+++ Core_Java/src/com/xworkz/core_java/collectionFramework/map/MapDemo.java
@@ -0,0 +1,33 @@
+package com.xworkz.core_java.collectionFramework.map;
+
+import java.util.HashMap;
+
+public class MapDemo {
+	public static void main(String[] args) {
+		HashMap hs=new HashMap();
+		hs.put(10, "Java");
+		hs.put(20,"Sql");
+		hs.put(30, "Web");
+		hs.put(40, "Program");
+		hs.put(50, "Aplication");
+		
+		System.out.println(hs);
+		System.out.println("***********");
+		System.out.println(hs.get(40));
+		System.out.println("***********");
+		System.out.println(hs.containsKey(20));
+		System.out.println("***********");
+        System.out.println(hs.containsValue("Java"));
+        System.out.println(hs.remove(10));
+        System.out.println(hs);
+        System.out.println(hs.isEmpty());
+        hs.clear();
+        System.out.println(hs.isEmpty());
+  
+	
+		
+		
+	}
+
+}
+
---
+++ Core_Java/src/com/xworkz/core_java/collectionFramework/map/MapDemo1.java
@@ -0,0 +1,47 @@
+package com.xworkz.core_java.collectionFramework.map;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class MapDemo1 {
+	public static void main(String[] args) {
+		HashMap<Integer,String> hs=new HashMap<Integer,String>();
+		hs.put(5, "Pen");
+		hs.put(6, "Book");
+		hs.put(7, "bag");
+		hs.put(8, "Bottel");
+		
+       Set<Integer> s=hs.keySet();
+       for(int key:s) {
+    	   System.out.println(key);
+    	   System.out.println("****************");
+    	   
+    	   LinkedHashMap<Integer, String> lhp=new LinkedHashMap<Integer, String>();
+    	   lhp.put(10, "Moblie");
+    	   lhp.put(20, "Tv");
+    	   lhp.put(30, "Pan");
+    	   
+          Set<Integer> s1=lhp.keySet();
+         for(int key1:s1) {
+        	 System.out.println(key1);
+        	 System.out.println("***************");
+        	 
+        	 
+        	 TreeMap<String , Integer> tm=new TreeMap<String, Integer>();
+        	 tm.put("Car", 100);
+        	 tm.put("Bike",200);
+        	 tm.put("Cycle", 300);
+        	Set<String> s2= tm.keySet();
+        	for(String key2: s2) {
+        		System.out.println(key2);
+        	}
+         }
+    	   
+       }
+
+	}
+
+}
